59f6d05c97b5386ad5936bf5c649699649b85731
Mark Wong <markwong.digital@gmail.com>
2026-03-28 23:38:13 -0700

    chore(client): init shadcn and add sidebar-07 block with dependencies

--- client/src/components/AppProviders.tsx
+++ client/src/components/AppProviders.tsx
@@ -1,6 +1,13 @@
 import type { WithChildrenProps } from "@/types/react";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { TooltipProvider } from "./ui/tooltip";
+
 function AppProviders({ children }: WithChildrenProps) {
-	return <>{children}</>;
+	return (
+		<ClerkProvider>
+			<TooltipProvider>{children}</TooltipProvider>
+		</ClerkProvider>
+	);
 }
 export default AppProviders;
